db5ca8b492697b16ef1ce2e9b5bd5d3b299d77b9
slidrex <108924768+slidrex@users.noreply.github.com>
2023-01-10 14:52:47 +0500

    melee weapon, weapon object

--- ApplePay/Assets/Scripts/Weapons/MeleeWeapon.cs
+++ ApplePay/Assets/Scripts/Weapons/MeleeWeapon.cs
@@ -1,20 +1,48 @@
 using UnityEngine;
 
-public class MeleeWeapon : WeaponObject
+public class MeleeWeapon : WeaponObject, ICollideDamageDealer, IHitResponder
 {
+    [Header("Collide settings")]
+    public PayHitShape Collider;
+    private void Awake() => Collider?.AddResponder(this);
+    [SerializeField] private int collideDamage;
+    public int CollideDamage {get => collideDamage; set => collideDamage = value;}
+    public float Knockback;
+    public float KnockbackTime;
+    public DamageType DamageType;
     public override void Activate(Creature attacker, Vector2 originPosition, Vector2 attackPosition, Transform target, out Projectile projectile)
     {
         LinkAttacker(attacker);
         projectile = null;
     }
-    protected override void OnEntityHitEnter(Collider2D collision, Entity hitEntity)
+    protected virtual Vector2 GetCollisionNormal(Collider2D collider) => collider.ClosestPoint(transform.position) - (Vector2)transform.position;
+    protected virtual void OnColliderHit(Collider2D collider) {}
+    protected virtual void OnMissColliderHit(Collider2D collider) {}
+    protected virtual void OnEntityHitEnter(Collider2D collider, Entity hitEntity)
     {
-        base.OnEntityHitEnter(collision, hitEntity);
-        if(hitEntity.isKnockable == true)
-        {
+        if(hitEntity != Owner) DealCollideDamage(hitEntity, CollideDamage, Owner);
             hitEntity.ForceHandler?.Knock(transform.up.normalized * Knockback, Knockback, KnockbackTime);
-            
-            
+    }
+    public void DealCollideDamage(Entity entity, int damage, Creature dealer) => entity?.Damage(damage, DamageType, dealer);
+    protected override void Update()
+    {
+        Collider.CheckHit();
+    }    
+    public virtual void OnHitDetected(HitInfo hitInfo)
+    {
+        OnColliderHit(hitInfo.collider);
+        if(hitInfo.entity != Owner)
+        {
+            OnEntityHitEnter(hitInfo.collider, hitInfo.entity);
+            Collider.IgnoreShape(hitInfo.entity.HitShape);
         }
+        else
+            OnMissColliderHit(hitInfo.collider);
+    }
+
+    public enum AttackSide
+    {
+        Attacker,
+        Attacked
     }
 }
--- ApplePay/Assets/Scripts/Weapons/WeaponObject.cs
+++ ApplePay/Assets/Scripts/Weapons/WeaponObject.cs
@@ -1,46 +1,8 @@
 using UnityEngine;
-public abstract class WeaponObject : MonoBehaviour, ICollideDamageDealer, IHitResponder
-{
-    [Header("Collide settings")]
-    public PayHitShape Collider;
-    private void Awake() => Collider?.AddResponder(this);
-    [SerializeField] private int collideDamage;
-    public int CollideDamage {get => collideDamage; set => collideDamage = value;}
-    public float Knockback;
-    public float KnockbackTime;
-    [Header("Weapon settings")]
-    protected Creature Owner;
-    public DamageType DamageType;
-    
-    protected virtual Vector2 GetCollisionNormal(Collider2D collider) => collider.ClosestPoint(transform.position) - (Vector2)transform.position;
-    protected virtual void OnColliderHit(Collider2D collider) {}
-    protected virtual void OnMissColliderHit(Collider2D collider) {}
-    protected virtual void OnEntityHitEnter(Collider2D collider, Entity hitEntity)
-    {
-        if(hitEntity != Owner) DealCollideDamage(hitEntity, CollideDamage, Owner);
-    }
-    public void DealCollideDamage(Entity entity, int damage, Creature dealer) => entity?.Damage(damage, DamageType, dealer);
+public abstract class WeaponObject : MonoBehaviour
+{    
+    public Creature Owner;
     public void LinkAttacker(Creature attacker) => Owner = attacker;
     public virtual void Activate(Creature attacker, Vector2 originPosition, Vector2 attackPosition, Transform target, out Projectile projectile) => projectile = null;
-    protected virtual void Update()
-    {
-        Collider.CheckHit();
-    }
-    public virtual void OnHitDetected(HitInfo hitInfo)
-    {
-        OnColliderHit(hitInfo.collider);
-        if(hitInfo.entity != Owner)
-        {
-            OnEntityHitEnter(hitInfo.collider, hitInfo.entity);
-            Collider.IgnoreShape(hitInfo.entity.HitShape);
-        }
-        else
-            OnMissColliderHit(hitInfo.collider);
-    }
-
-    public enum AttackSide
-    {
-        Attacker,
-        Attacked
-    }
+    protected virtual void Update() { }
 }
